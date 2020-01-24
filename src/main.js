556e3774b4cc423a6f46aebb6a68e17f8b75cf63
0xbul1 <diaoszhao@gmail.com>
2020-01-24 18:12:21 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import store from './store'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// new Vue({
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+
+import Vue from 'vue';
+import App from './App.vue';
+
+// 1. 导入 ant-design-vue 组件库
+import Antd from 'ant-design-vue';
+// 2. 导入组件库的样式表
+import 'ant-design-vue/dist/antd.css';
+
+Vue.config.productionTip = false;
+// 3. 安装组件库
+Vue.use(Antd);
 
 new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
